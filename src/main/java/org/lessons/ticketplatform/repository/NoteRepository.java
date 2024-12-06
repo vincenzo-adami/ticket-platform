82d7bc1f5f2cb9749591beda1c95ac49445b6208
vincenzo-adami <51445073+vincenzo-adami@users.noreply.github.com>
2024-12-06 15:05:31 +0100

    ADD: Custom deletion for notes when ticket is deleted

--- src/main/java/org/lessons/ticketplatform/repository/NoteRepository.java
+++ src/main/java/org/lessons/ticketplatform/repository/NoteRepository.java
@@ -3,6 +3,10 @@ package org.lessons.ticketplatform.repository;
 import org.lessons.ticketplatform.model.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import jakarta.transaction.Transactional;
+
 public interface NoteRepository extends JpaRepository<Note, Long> {
 
+  @Transactional
+  void deleteByTicketId(Long id);
 }
